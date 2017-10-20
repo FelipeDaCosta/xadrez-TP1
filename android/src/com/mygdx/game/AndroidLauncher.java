7d5324b618846df1499811dc5a110487faaee189
Alex Siqueira Lacerda <Lequizi@gmail.com>
2017-10-20 12:20:17 -0200

    Mudanca completa no jogo

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.graphic.Chess;
 TP1;
 
 public class AndroidLauncher extends AndroidApplication {
